eb6c64977efc63cc25cdf0390996a24f19a533f2
Andre Pierre <andre.laurence94@gmail.com>
2020-08-12 00:12:14 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,8 +2,8 @@ def find_element_index(array, value_to_find)
   i = 0 
   
   while i < array.length do
-    if  array[i] == value_to_find
-  i
+    if array[i] == value_to_find
+      i 
     end 
   i += 1 
   end 
